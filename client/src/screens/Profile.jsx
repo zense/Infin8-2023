c3eea8f15671362421d9a19dee45014c33cabe42
KalyanRam1234 <kalyanrammunagala@gmail.com>
2023-01-13 22:14:45 +0530

    Title of Profile

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -2,10 +2,20 @@ import './Profile.scss'
 import qr from '../images/qr-ticket.png'
 import arrow from '../images/downrightwhite.svg'
 import RegisteredSlip from '../components/RegisteredSlip/RegisteredSlip'
+import Vector from '../images/Vector3.png'
 const Profile = ()=>{
     return <div className="Profile">
-        <div className="row title">
-
+        <div className="row title" style={{"marginTop": "13vw"}}>
+            <div className='col-12 YourProf'>
+               YOUR 
+            </div>
+        </div>
+        
+        <div className="row title" style={{"marginBottom": "100px"}}>
+            <div className='col-12 YourProf'>
+               PROFILE
+               <img src={Vector} alt="" className='arrowicon1' />
+            </div>
         </div>
 
         <div className="row page_heading">
