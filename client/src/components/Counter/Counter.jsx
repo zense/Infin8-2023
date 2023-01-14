5ebf808dbbdea9b6078ceacf84f48866d6c6beb6
RohitShah1706 <rlshah03@gmail.com>
2023-01-14 19:36:22 +0530

    countdown now updates itself

--- client/src/components/Counter/Counter.jsx
+++ client/src/components/Counter/Counter.jsx
@@ -1,23 +1,52 @@
 import "./Counter.scss"
-const Counter = () => {
+import { useState, useEffect } from "react";
+const Counter = ({dayshoursMinSecs}) => {
+    const { das=0, hours = 0, minutes = 0, seconds = 0 } = dayshoursMinSecs;
+    const [[days, hrs, mins, secs], setTime] = useState([das, hours, minutes, seconds]);
+
+    const tick = () => {
+        if(days==0 && hrs === 0 && mins === 0 && secs === 0){
+            reset()
+        }
+        else if (hrs === 0 && mins === 0 && secs === 0) {
+            setTime([days - 1, 23, 59, 59]);
+        }
+        else if (mins === 0 && secs === 0) {
+            setTime([days, hrs - 1, 59, 59]);
+        }
+        else if (secs === 0) {
+            setTime([days, hrs, mins - 1, 59]);
+        }
+        else {
+            setTime([days, hrs, mins, secs - 1]);
+        }
+    };
+
+    const reset = () => setTime([parseInt(das),parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+    useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
     return(
         <div className="square-container">
             <div className="row">
                 <div className="col-4 col-xl-3">
                     <div className="square-orange">
-                        <div className="square-text">24</div>
+                        <div className="square-text">{days}</div>
                         <div className="square-subtitle ">Days</div>
                     </div>
                 </div>
                 <div className="col-4 col-xl-3 ">
                     <div className="square-orange">
-                        <div className="square-text">00</div>
+                        <div className="square-text">{hrs}</div>
                         <div className="square-subtitle ">Hours</div>
                     </div>
                 </div>
                 <div className="col-4 col-xl-3 ">
                     <div className="square-orange">
-                        <div className="square-text">59</div>
+                        <div className="square-text">{mins}</div>
                         <div className="square-subtitle ">Mins</div>
                     </div>
                 </div>
--- client/src/components/Title/Title.jsx
+++ client/src/components/Title/Title.jsx
@@ -3,7 +3,20 @@ import Infin8 from "../../images/Infin8_black_text.svg";
 import Infin8white from "../../images/Infin8_white_text.png";
 import year from "../../images/2023_text.svg";
 import Counter from "../Counter/Counter"
+import { useEffect, useState } from "react";
 const Title = ()=>{
+    const [dayshoursMinSecs, setdayshoursMinSecs] = useState({das:0, hours:0, minutes: 0, seconds: 0});
+    const finalDate = new Date("2023-02-07T00:00:00");
+
+    useEffect(() => {
+        const diffDate = finalDate - new Date();
+        const days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
+        console.log(days, hours, minutes, seconds)
+        setdayshoursMinSecs({das:days, hours:hours, minutes: minutes, seconds: seconds})
+    },[])
     return <div className="Title">
         <div className="row">
             <div className="col-12 col-sm-6">
@@ -15,7 +28,7 @@ const Title = ()=>{
                 </div>
             </div>
         </div>
-        <Counter />
+        <Counter dayshoursMinSecs={dayshoursMinSecs}/>
     </div>
 };
 
