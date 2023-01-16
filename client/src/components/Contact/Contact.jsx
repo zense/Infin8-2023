88fa4875563866a71e4f6cde55cff32ee8159862
Vikas Kalyanapuram <vikaskaly@Vikass-MacBook-Air.local>
2023-01-16 23:57:12 +0530

    Making Heading of contact page responsive

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,44 +2,20 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import React from "react";
 import './Contact.scss';
-function ContactCard(props){
-    return (
-        <div className="col-7">
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="person">Person 1</div>
-                    <div className="extra">+91 99999 99999 </div>
-                    <div className="extra">person@gmail.com </div>
-                </div>
-                <div className="col-md-2"></div>
-                <div className="col-md-4"></div>
-            </div>
-        </div>
-    )
-}
-
 
 export default function Contact(){
     return (
         <>
             <Navbar />
-            <div className="ContactPage">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="Contact row titlerow">
-                            CONTACT US
+                <div className="Contacts_style ContactPage" >
+                    <div className="row">
+                        <div className="col-12 col-md-7" >
+                            <div className="row titlerow">
+                                CONTACT US
+                            </div>
                         </div>
-                    
-                    </div>
-                    <div>
-
                     </div>
                 </div>
-                
-                <div className="row">
-                    <ContactCard />
-                </div>
-            </div>
             <Footer />
         </>
     )
