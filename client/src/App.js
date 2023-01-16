278ad5f622cbff7fec97fa012b3017518566d0bd
KalyanRam1234 <kalyanrammunagala@gmail.com>
2023-01-16 21:56:17 +0530

    Working on contact page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Profile from "./screens/Profile";
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
+import Contact from "./components/Contact/Contact";
 const ScrollToTop = (props) => {
   const location = useLocation();
   useEffect(() => {
@@ -29,6 +30,7 @@ function App() {
             {/* If the user is not signed in, paid_base_fees has to be given as false if we want the pay Base Fees page to render  */}
             <Route path="/registerevent/:id" element={<RegisterEvent paid_base_fees={true} signed_in={true}/>} />
             <Route path="/profile" element = {<Profile/>}/>
+            <Route path="/contact" element={<Contact/>} />
             <Route path="*" element={<Navigate to ="/home" replace/>} />
           </Routes>
           </ScrollToTop>
