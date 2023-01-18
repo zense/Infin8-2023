3cf6370a897786f6cea697c40495691010b494d4
unbalancedvariance <dhanvimedha@gmail.com>
2023-01-18 20:03:19 +0530

    some minor changes

--- client/src/components/RegisterEvent/RegisterTeam/RegisterTeam.jsx
+++ client/src/components/RegisterEvent/RegisterTeam/RegisterTeam.jsx
@@ -3,18 +3,132 @@ import Dotted from "../../../images/dottedbox.png"
 import './RegisterTeam.css'
 import { BsFillSquareFill } from "react-icons/bs";
 import React from 'react'
+import { db } from "../../../firebase-config";
+import { collection, doc, getDocs, getDoc, updateDoc, addDoc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createRouter } from "@remix-run/router";
+
+
 export default function RegisterTeam(props){
+
     const [joinExistingTeam,changejoinExistingTeam] = React.useState(true);
     //const [teamCodeInput, changeTeamCodeInput] = React.useState(true);
-    const [color1, changecolor1] = React.useState("red");
-    const [color2, changecolor2] = React.useState("green");
+    const [color1, changecolor1] = useState("red");
+    const [color2, changecolor2] = useState("green");
+
+    const [paid_base_fees, set_paid_base_fees] = useState(false);
+    const [user_registered, set_user_registered] = useState(false);
+    const [teamID, setTeamID] = useState("");
+
+    const checkStatus = async () => {
+        var userReference = doc(db, "users_list", props.user_id);
+        var userData = getDoc(userReference);
+
+        var baseFeesPaid = (await userData).data().paid_base_fees;
+        // var paymentDetails = (await userData).data().paymentDetails;
+        
+        if (baseFeesPaid){
+            set_paid_base_fees(true);
+            document.getElementById("chooseTeam").style.visibility = "visible";
+            // console.log("Paid base fee :", paid_base_fees); 
+        }
+        else{
+            document.getElementById("chooseTeam").style.visibility = "hidden";
+        }
+        // if (paymentDetails[props.event_id] !== "Register"){
+        //     var paymentReference = doc(db, "payments", paymentDetails[props.event_id]);
+        //     var paymentData = getDoc(paymentReference);
+            
+        //     var status = (await paymentData).data().status;
+    
+        //     if (status === "processed"){
+        //         set_user_registered(true);
+        //         // document.getElementById("payBaseFeeButton").classList.add("disabled");
+        //         // props.setDisplayText("Payment Successful");
+        //         // return true;
+        //     }
+        //     // else{
+        //         // set_user_registered(f)
+        //         // document.getElementById("payBaseFeeButton").innerHTML = "Processing";
+        //         // document.getElementById("payBaseFeeButton").classList.add("disabled");
+        //         // return false;
+        //     // }
+        // }
+
+    }
+
+    if (props.loggedInStatus){
+        checkStatus();
+    }
+
+    let navigate = useNavigate(); 
+    const goToPayBaseFeePage = () =>{
+        navigate(`/pay-base-fees`);
+    }
+
+
+    const createPaymentObject = async () => {
+
+        // Create a new team
+        if (!joinExistingTeam){
+
+            console.log(props.user_id);
+            // event id
+            const paymentRef = await addDoc(collection(db, "payments"), {
+                event_id: props.event_id,
+                multi: true,
+                screenshot: "",
+                status:"processing",
+                transaction_id:"ID NUMBER PATA NAHI",
+                user: props.user_id
+            });
+            console.log("Payment Object Made");
+            
+            let paymentObjectID = paymentRef.id;
+
+            const userRef = doc(db, "users_list", props.user_id);
+            const userDocSnap = getDoc(userRef);
+
+            console.log("Setting users_list");
+
+            // if (userDocSnap.exists()){
+            let paymentDetails = (await userDocSnap).data().paymentDetails;
+            paymentDetails[props.event_id] = paymentObjectID;
+
+            updateDoc(userRef, {
+                paymentDetails: paymentDetails
+            })
+
+            
+            const teamData = await addDoc(collection(db, "teams"), {
+                limit: 5,
+                vacancy: 4,
+                members: [props.user_id]
+            });
+
+            setTeamID(teamData.id);
+            // document.getElementById("payBaseFeeButton").innerHTML = "Processing";
+            document.getElementsByName("RegisterForEvent")[0].classList.add("disabled");
+        }
+        else{
+            const inputTeamID = document.getElementById("inputID").value;
+            
+            // get all teams and check if it matches any of the ids
+        
+        }
+        
+
+    }
+
+
     return (
         
         <div>
             <div style={{"textAlign":"center"}}>
                 <h1 style={{"color":"white","paddingTop":"32px", "paddingBottom":"10px","fontSize":"3rem","fontFamily":"Archivo","fontWeight":"700"}}>REGISTER</h1>
             </div>
-            <div className="container-fluid" style={{"paddingLeft":"2.7vw", "paddingRight":"2.7vw"}}>
+            <div id="chooseTeam" className="container-fluid" style={{"paddingLeft":"2.7vw", "paddingRight":"2.7vw"}}>
                 <div className="row">
 
                     <div className="btn-group btn-group-lg" role="group" aria-label="Basic radio toggle button group" style={{"marginTop":"10px"}}>
@@ -85,15 +199,19 @@ export default function RegisterTeam(props){
                 <span>Total Fee</span><span style={{"float":"right","paddingRight":"50px"}}>{!joinExistingTeam ? "Rs." + props.fee : "Free"}</span></div>
             </div>
 
+            {paid_base_fees === true 
+            ?
+                <div>
+
             {!joinExistingTeam &&
 
                 <div>
                     <div style={{"color":"white","marginTop":"20px"}}>
                         <h1 style={{"textAlign":"center","fontSize":"3rem","fontFamily":"Archivo","fontWeight":"700"}}>PAYMENT</h1>
                         <div style={{"marginLeft":"15px","marginRight":"15px",  "marginTop": "15px", "marginBottom": "15px"}}>Pay the above mentioned amount using UPI and 
-                        upload the receipt screenshot here. Our team will 
-                        verify the payment. The 'Register' button will turn 
-                        to 'Registered ' if it is approved :</div>
+                    upload the receipt screenshot here, making sure that the UPI reference ID is visible. Our team will 
+                    verify the payment. The 'Register' button will turn 
+                    to 'Registered ' if it is approved :</div>
                     </div>
 
                     <div className="fluid-container">
@@ -173,55 +291,25 @@ export default function RegisterTeam(props){
                         />}
                 </div>
 
-                {props.paid_base_fees === false 
-                ?
-                <div>
+                
                     <button 
-                        disabled
+                        name="RegisterForEvent"
                         className="btn btn-default" 
                         style={{"backgroundColor":"white","marginTop":"25px"}}
-                        
-                        onClick={()=>{
-                            if(props.paid_base_fees==false){
-                                window.location.href='/pay_base_fees'
-                            }
-                        }}>Register</button>
-                    
-                    <br></br>
-                    
-                    <button 
-                        className="btn btn-default" 
-                        style={{"backgroundColor":"white","marginTop":"25px"}}
-
-                        onClick={()=>{
-                            window.location.href='/pay_base_fees'
-                        }}>Pay Base Fee</button>
+                        onClick={createPaymentObject}>Register</button>
+                </div>
                 </div>
                 :
                 <div>
                     <button 
                         className="btn btn-default" 
                         style={{"backgroundColor":"white","marginTop":"25px"}}
-                        
-                        onClick={()=>{
-                            if(props.paid_base_fees==false){
-                                window.location.href='/pay_base_fees'
-                            }
-                        }}>Register</button>
+                        onClick={goToPayBaseFeePage}>Pay Base Fee</button>
                     
-                    {/* <button 
-                        className="btn btn-default" 
-                        style={{"backgroundColor":"white","marginTop":"25px"}}
-
-                        onClick={()=>{
-                            window.location.href='/pay_base_fees'
-                        }}>Pay Base Fee</button> */}
                 </div>}
               
                     
             </div>
-            
-        </div>
         
     );
 }
