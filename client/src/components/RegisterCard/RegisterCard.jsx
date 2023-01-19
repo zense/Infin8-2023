e42c1342b37cb323d6c4b2c6bfff904df274ff00
unbalancedvariance <dhanvimedha@gmail.com>
2023-01-19 08:37:25 +0530

    added backend and some minor changes in design!

--- client/src/components/RegisterCard/RegisterCard.jsx
+++ client/src/components/RegisterCard/RegisterCard.jsx
@@ -11,6 +11,7 @@ import { Zoom } from "react-reveal";
 import eventDetails from "../../content/eventDetails.json";
 import { useEffect } from "react";
 import { useLocation } from "react-router";
+import {BiRupee} from 'react-icons/bi'
 const ScrollToTop = (props) => {
     const location = useLocation();
     useEffect(() => {
@@ -20,6 +21,7 @@ const ScrollToTop = (props) => {
     return <>{props.children}</>
 };
 function LeftCard(props) {
+    var link="./eventimages/"+ props.props.id+".jpg";
     return (
         <>
             <Zoom duration={1500} delay={100} >
@@ -27,7 +29,7 @@ function LeftCard(props) {
                     <div className="row ">
                         <div className="col-md-1"></div>
                         <div className="col-md-10 LeftCards" style={{ "padding": "0px" }}>
-                            <img src={DUMMY} className="val"></img>
+                            <img src={link} className="val"></img>
                         </div>
                         <div className="col-md-1" ></div>
                     </div>
@@ -43,7 +45,7 @@ function LeftCard(props) {
                         <div className="col-md-1"></div>
                         <div className="col-md-10">
                             <div className="row">
-                                <Link to={`#`} className="col-6 link edge popup" > <div>View Details</div> </Link>
+                                <Link to={`/registerevent/${props.props.id}`} className="col-6 link edge popup" > <div>View Details</div> </Link>
                                 <Link to={
                                     `/registerevent/${props.props.id}`
                                 } className="col-6 edge link popup" style={{ borderLeft: 0 }} > <div>Register</div> </Link>
@@ -54,14 +56,14 @@ function LeftCard(props) {
                 </div>
                 <div className="col-12 SmallCard">
                     <div className="row">
-                        <div className="col-6 edge" style={{ "padding": "0px" }}><img src={DUMMY} className="val"></img></div>
+                        <div className="col-6 edge" style={{ "padding": "0px" }}><img src={link} className="val"></img></div>
                         <div className="col-6 edge" style={{ borderLeft: 0 }}>
                             <div className="Titl"> {props.title}</div>
                             <div className="incen">{props.Incentives}</div>
                         </div>
                     </div>
                     <div className="row" >
-                        <Link to={`#`} className=" col-6 edge1 link" style={{ borderTop: 0 }}>
+                        <Link to={`/registerevent/${props.props.id}`} className=" col-6 edge1 link" style={{ borderTop: 0 }}>
                             <div >
                                 Details
                                 <img src={ARROW} className="arrowpoint"></img>
@@ -87,6 +89,22 @@ function RegisterCard(props) {
             <ScrollToTop>
                 <Navbar props={props}></Navbar>
                 <Title></Title>
+                <div className="row">
+            <Zoom delay={500} duration={1250}>
+            {/* {props.paid_base_fees==false ? <div className='col-12 Disclaimer' style={{"backgroundColor":"#474646","padding":"20px 20px 20px 20px","color":"white","fontFamily":"Archivo"}}>
+                    <div>
+                        To register for an event, the base fee of <BiRupee style={{"marginTop": "-5px"}}/>50 needs to be paid. Once this fee is paid, you can register for any event in this fest. 
+                        <div style={{"marginTop" : "10px"}}>You an pay the base fee <a href="/pay_base_fees" style={{ "color" : "white"}}>here </a></div>
+                    </div>
+                </div> : null} */}
+                <div className='col-12 Disclaimer' style={{"backgroundColor":"#474646","padding":"20px 20px 20px 20px","color":"white","fontFamily":"Archivo"}}>
+                    <div>
+                      All non-participants will have to pay an Entrance fee of <BiRupee style={{"marginTop": "-5px"}}/><b>50</b> before entering the campus.
+                    </div>
+                </div> 
+                {/* disclaimer should be displayed only if the paid_base_fee isn't paid, else it shouldn't be visible, backend peeps, pls take care of this */}
+                </Zoom>
+            </div>
                 <div className="container-fluid RegisterCard">
                     <div className="row marginchange invert">
                         {
