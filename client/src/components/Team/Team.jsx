01fb3e63e036fe3f6b0c226ee452dc428f1d815f
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-01-22 10:17:58 +0530

    Team page

--- client/src/components/Team/Team.jsx
+++ client/src/components/Team/Team.jsx
@@ -5,7 +5,7 @@ import CALL from "../../images/call.webp";
 import {Zoom} from "react-reveal";
 import { Fade } from "react-reveal";
 import Vector from '../../images/Vector3.png'
-import { BsInstagram } from 'react-icons/bs';
+import { BsGithub, BsInstagram } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
 import './NewTeam.scss'
 function NewTeamCard(props){
@@ -23,7 +23,7 @@ function NewTeamCard(props){
                     <div className="display-table change">
                         <div className="display-table-cell ">
                             <div >
-                               <a href={props.insta} style={{"text-decoration": "none" , "color": "black"}}> <BsInstagram className="newicons"/></a> <a href={props.linkedin} style={{"text-decoration": "none" , "color": "black"}}> <BsLinkedin className="newicons"/></a> 
+                               <a href={props.insta} style={{"text-decoration": "none" , "color": "black"}}> <BsInstagram className="newicons"/></a> {props.github && <a href={props.github} style={{"text-decoration": "none" , "color": "black"}}> <BsGithub className="newicons"/></a> }
                             </div>
                             <div className="team-details">
                                 <h4 className="team-name">
@@ -49,14 +49,14 @@ function Template(props){
                     </div>
                 </div>
                 <div className="row">
-                    <NewTeamCard img={"./eventimages/Chaitanya.png"} name={"Chaitanya Manas"} role={"SAC"} insta={"https://instagram.com/cmanas03?igshid=NTdlMDg3MTY="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Jacob.jpg"} name={"Jacob Matthew"} role={"SAC"} insta={""} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Asrith.jpeg"} name={"Asrith Nune"} role={"SAC"} insta={"https://www.instagram.com/asrith_gupta/"} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Gowtham.jpg"} name={"Gowtham Reddy"} role={"SAC"} insta={"https://instagram.com/_.hgangster._?igshid=NTdlMDg3MTY="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Sahithi.jpg"} name={"V L Sahithi"} role={"SAC"} insta={"https://www.instagram.com/vlsahithi/"} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Gopal.jpeg"} name={"Gopal Gupta"} role={"SAC"} insta={""} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Rahul.jpeg"} name={"Rahul Tejpal"} role={"SAC"} insta={"https://www.instagram.com/rahultejpal7/"} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Debag.jpeg"} name={"Debagana Mukherjee"} role={"SAC"} insta={""} linkedin={""}/>
+                    <NewTeamCard img={"./eventimages/Chaitanya.png"} name={"Chaitanya Manas"} role={"SAC"} insta={"https://instagram.com/cmanas03?igshid=NTdlMDg3MTY="} github={""}/>
+                    <NewTeamCard img={"./eventimages/Jacob.jpg"} name={"Jacob Matthew"} role={"SAC"} insta={""} github={""}/>
+                    <NewTeamCard img={"./eventimages/Asrith.jpeg"} name={"Asrith Nune"} role={"SAC"} insta={"https://www.instagram.com/asrith_gupta/"} github={""}/>
+                    <NewTeamCard img={"./eventimages/Gowtham.jpg"} name={"Gowtham Reddy"} role={"SAC"} insta={"https://instagram.com/_.hgangster._?igshid=NTdlMDg3MTY="} github={""}/>
+                    <NewTeamCard img={"./eventimages/Sahithi.jpg"} name={"V L Sahithi"} role={"SAC"} insta={"https://www.instagram.com/vlsahithi/"} github={""}/>
+                    <NewTeamCard img={"./eventimages/Gopal.jpeg"} name={"Gopal Gupta"} role={"SAC"} insta={""} github={""}/>
+                    <NewTeamCard img={"./eventimages/Rahul.jpeg"} name={"Rahul Tejpal"} role={"SAC"} insta={"https://www.instagram.com/rahultejpal7/"} github={""}/>
+                    <NewTeamCard img={"./eventimages/Debag.jpeg"} name={"Debagana Mukherjee"} role={"SAC"} insta={"https://instagram.com/deegchin?igshid=NTdlMDg3MTY="} github={""}/>
                 </div>
 
                 <div className="row">
@@ -65,13 +65,14 @@ function Template(props){
                     </div>
                 </div>
                 <div className="row">
-                    <NewTeamCard img={"./eventimages/Rohit.jpg"} name={"Rohit Shah"} role={"SPOC/Backend-Developer"} insta={"https://instagram.com/mr_rohit_shah_?igshid=NTdlMDg3MTY="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Dhanvi.jpeg"} name={"Dhanvi Medha"} role={"SPOC/Backend-Developer"} insta={"https://www.instagram.com/dhanvi.medha/"} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Subhajeet.jpeg"} name={"Subhajeet Lahiri"} role={"Frontend-Developer"} insta={"https://instagram.com/subhajeetlahiri?igshid=NTdlMDg3MTY="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Kalyan.jpg"} name={"Kalyan Ram"} role={"Frontend-Developer"} insta={"https://instagram.com/kalyanrammunagala?igshid=NmQ2ZmYxZjA="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Vikas2.jpeg"} name={"Vikas Kalyanapuram"} role={"Frontend-Developer"} insta={"https://instagram.com/vkas_13?igshid=NTdlMDg3MTY="} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Amar.jpg"} name={"Amar Pratap Singh"} role={"Backend-Developer"} insta={"https://www.instagram.com/__amar012/"} linkedin={""}/>
-                    <NewTeamCard img={"./eventimages/Amar.jpg"} name={"Rishi Dutt"} role={"UI/UX Developer"} insta={""} linkedin={""}/>
+                    <NewTeamCard img={"./eventimages/Rohit.jpg"} name={"Rohit Shah"} role={"SPOC/Backend-Developer"} insta={"https://instagram.com/mr_rohit_shah_?igshid=NTdlMDg3MTY="} github={"https://github.com/RohitShah1706"}/>
+                    <NewTeamCard img={"./eventimages/Dhanvi.jpeg"} name={"Dhanvi Medha"} role={"SPOC/Backend-Developer"} insta={"https://www.instagram.com/dhanvi.medha/"} github={"https://github.com/unbalancedvariance"}/>
+                    <NewTeamCard img={"./eventimages/Rishi.jpeg"} name={"Rishi Dutt"} role={"UI/UX Developer"} insta={""} github={""}/>
+                    <NewTeamCard img={"./eventimages/Subhajeet.jpeg"} name={"Subhajeet Lahiri"} role={"Frontend-Developer"} insta={"https://instagram.com/subhajeetlahiri?igshid=NTdlMDg3MTY="} github={"https://github.com/Heliospook"}/>
+                    <NewTeamCard img={"./eventimages/Kalyan.jpg"} name={"Kalyan Ram"} role={"Frontend-Developer"} insta={"https://instagram.com/kalyanrammunagala?igshid=NmQ2ZmYxZjA="} github={"https://github.com/KalyanRam1234"}/>
+                    <NewTeamCard img={"./eventimages/Vikas2.jpeg"} name={"Vikas Kalyanapuram"} role={"Frontend-Developer"} insta={"https://instagram.com/vkas_13?igshid=NTdlMDg3MTY="} github={"https://github.com/LieutPaul"}/>
+                    <NewTeamCard img={"./eventimages/Amar.jpg"} name={"Amar Pratap Singh"} role={"Backend-Developer"} insta={"https://www.instagram.com/__amar012/"} github={"https://github.com/Amar-Pratap-Singh"}/>
+                    
                 </div>
             </div>
         </div>
